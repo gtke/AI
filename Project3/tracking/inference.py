c31f9c7af5eae81eea3eead9142fd3c8e302c6b4
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-10-27 01:24:13 -0400

    problem 1 passes all tests

--- Project3/tracking/inference.py
+++ Project3/tracking/inference.py
@@ -146,7 +146,6 @@ class ExactInference(InferenceModule):
         pacmanPosition = gameState.getPacmanPosition()
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
 
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
@@ -154,9 +153,10 @@ class ExactInference(InferenceModule):
         allPossible = util.Counter()
         for p in self.legalPositions:
             trueDistance = util.manhattanDistance(p, pacmanPosition)
-            if emissionModel[trueDistance] > 0: allPossible[p] = 1.0
+            if emissionModel[trueDistance] > 0: allPossible[p] = emissionModel[trueDistance] * self.beliefs[p]
 
-        "*** END YOUR CODE HERE ***"
+        if noisyDistance == None:
+            allPossible[self.getJailPosition()] = 1.0
 
         allPossible.normalize()
         self.beliefs = allPossible
