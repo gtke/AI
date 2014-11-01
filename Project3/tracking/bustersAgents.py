f6e57384c26d99c08116eeeb168a46e7fa41d79b
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-11-01 00:14:22 -0400

    problem 3 implemented / passes tests

--- Project3/tracking/bustersAgents.py
+++ Project3/tracking/bustersAgents.py
@@ -157,4 +157,22 @@ class GreedyBustersAgent(BustersAgent):
                                             in enumerate(self.ghostBeliefs)
                                             if livingGhosts[i+1]]
         "*** YOUR CODE HERE ***"
+        bestAction = None
+        bestActionDist = None
+        for dist in livingGhostPositionDistributions:
+            mostLikelyPos = None
+            mostLikelyProb = None
+            for pos, prob in dist.items():
+                if mostLikelyProb == None or prob > mostLikelyProb:
+                    mostLikelyPos = pos
+                    mostLikelyProb = prob
+
+            for action in legal:
+                newPos = Actions.getSuccessor(pacmanPosition, action)
+                d = self.distancer.getDistance(newPos, mostLikelyPos)
+                if bestAction == None or d < bestActionDist:
+                    bestAction = action
+                    bestActionDist = d
+
+        return bestAction
         util.raiseNotDefined()
